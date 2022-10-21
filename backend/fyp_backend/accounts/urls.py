06bc8f4042a2d05cb5282a36452396336f6aeed5
adam10426 <adamabid521@gmail.com>
2022-10-20 18:22:24 -0700

    setting accounts app

---
+++ backend/fyp_backend/accounts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import re_path,path
+from .views import *
+
+app_name = 'accounts'
+
+urlpatterns = []
---
+++ backend/fyp_backend/accounts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from rest_framewok.response import response
+from rest_framework.views import APIVIEW
+from rest_framewok import status
+
+# Create your views here.
+
